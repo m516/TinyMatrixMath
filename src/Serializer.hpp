69ab55af3dfc0077b6ffc832d2735d922578a344
M516 <micah112358@gmail.com>
2023-06-08 07:55:11 -0500

    Test Serializer Arduino code hasn't been tested yet

--- src/Serializer.hpp
+++ src/Serializer.hpp
@@ -18,6 +18,8 @@ using uint8_t = std::uint8_t;
 namespace serializer{
 
 
+    /// @brief A general purpose basic serializer and deserializer for any instantiable data type.
+    /// @tparam T the data type to serialize and deserialize
     template<typename T>
     class Serializer{
 
@@ -31,16 +33,32 @@ namespace serializer{
         uint8_t  suffix = 'z';
         uint16_t checksum;
 
-        /// @brief Creates a 
-        /// @param object 
-        /// @param id 
+        /// @brief Creates a Serializer that can deserialize and serialize 
+        /// objects and immediately serialize "object" with a call to "serialize"
+        /// @param object an object to serialize
+        /// @param id the unique ID of this Serializer. Make sure it equals the ID of the receieving deserializer.
         Serializer(const T &object, uint8_t id) : id(id), object(object){
             checksum = compute_checksum();
         }
 
+        /// @brief Creates a Serializer that can deserialize and serialize objects
+        /// @param id 
+        Serializer(uint8_t id) : id(id), object(T()){
+            checksum = compute_checksum();
+        }
+        
         /// @brief Sends the object over a byte stream by repeatedly calling the "send_byte" function
         /// @tparam Return_Type the return type of the send_byte function
-        /// @param send_byte a function that sends a byte over a stream of some sort
+        /// @param send_byte a function that sends a byte over a stream of some sort. 
+        /// @note 
+        /// If using a method of a class (i.e. Class instance; instance.method) invoke it like this:     <br>
+        /// ~~~~~~~~~~~~~~~{.cpp}
+        /// serialize_instance.serialize<Class, void>(instance, &Class::method);
+        /// ~~~~~~~~~~~~~~~
+        /// For example, to use Serial.write, serialize like this:                                       <br>
+        /// ~~~~~~~~~~~~~~~{.cpp}
+        /// serialize_instance.serialize<Stream, void>(Serial, &Stream::write);
+        /// ~~~~~~~~~~~~~~~
         template<typename Return_Type>
         void serialize(Return_Type (*send_byte)(uint8_t)){
             // Serialization is done in five stages:
@@ -60,13 +78,46 @@ namespace serializer{
             send_byte(checksum>>8); // msb
         }
 
+
+        /// @brief Sends the object over a byte stream by repeatedly calling the "send_byte" function
+        /// @tparam Return_Type the return type of the send_byte function
+        /// @param send_byte a function that sends a byte over a stream of some sort
+        /// @note 
+        /// If using a method of a class (i.e. Class instance; instance.method) invoke it like this:     <br>
+        /// ~~~~~~~~~~~~~~~{.cpp}
+        /// serialize_instance.serialize<Class, void>(instance, &Class::method);
+        /// ~~~~~~~~~~~~~~~
+        /// For example, to use Serial.write, serialize like this:                                       <br>
+        /// ~~~~~~~~~~~~~~~{.cpp}
+        /// serialize_instance.serialize<Stream, void>(Serial, &Stream::write);
+        /// ~~~~~~~~~~~~~~~
+        template<typename Object_type, typename Return_Type>
+        void serialize(Object_type &send_object, Return_Type (Object_type::*send_byte)(uint8_t)){
+            // Serialization is done in five stages:
+            // 1. The prefix
+            (send_object.*send_byte)(prefix);
+            // 2. The id
+            (send_object.*send_byte)(id);
+            // 3. The object
+            uint8_t * ptr = (uint8_t *) &object;
+            for(int i = 0; i < sizeof(T); i++){
+                (send_object.*send_byte)(ptr[i]);
+            }
+            // 4. The suffix
+            (send_object.*send_byte)(suffix);
+            // 5. The checksum
+            (send_object.*send_byte)(checksum);    // lsb
+            (send_object.*send_byte)(checksum>>8); // msb
+        }
+
         /// @brief Repeatedly call this function to receive the object over a byte stream.
-        /// @param byte the byte to receive
+        /// @param incoming_byte the byte to receive
+        /// @param write_object a pointer to the object to write the received object to (optional)
         /// @return a pointer to the object if the object has been received, or a null 
         /// pointer if the object has not been received or was corrupted in transit.
-        T* deserialize(uint8_t byte){
-            enum ParseStage { stage_prefix, stage_id, stage_object, stage_suffix, stage_checksum };
-            static ParseStage stage = prefix;
+        bool deserialize(uint8_t incoming_byte, T *destination_object = nullptr){
+            enum ParseStage { stage_prefix, stage_id, stage_object, stage_suffix, stage_checksum, stage_checksum_2 };
+            static ParseStage stage = stage_prefix;
             static uint8_t * ptr = (uint8_t *) &object;
 
             // Serialization is done in five stages:
@@ -83,12 +134,12 @@ namespace serializer{
 
             switch(stage){
                 case stage_prefix:                              // We expect the prefix
-                    if(byte == prefix){                         // If it matches
+                    if(incoming_byte == prefix){                // If it matches
                         stage = stage_id;                       //     Go to the next stage
                     }               
                     break;               
                 case stage_id:                                  // We expect the id
-                    if(byte == prefix){                         // If it matches
+                    if(incoming_byte == id){                    // If it matches
                         stage = stage_object;                   //    Go to the next stage
                     }               
                     else{                                       // If it doesn't match
@@ -96,14 +147,14 @@ namespace serializer{
                     }               
                     break;               
                 case stage_object:                              // We expect the object data itself
-                    ptr++ = byte;                               // Store the byte in the object and increment the pointer
-                    if(ptr >= object+sizeof(T)){                // If we've received all the bytes
+                    *(ptr++) = incoming_byte;                      // Store the byte in the object and increment the pointer
+                    if(ptr >= (uint8_t *) &object+sizeof(T)){   // If we've received all the bytes
                         ptr = (uint8_t *) & object;             //    Reset the pointer
                         stage = stage_suffix;                   //    Go to the next stage
                     }
                     break;
                 case stage_suffix:                              // We expect the suffix
-                    if(byte == suffix){                         // If it matches
+                    if(incoming_byte == suffix){                // If it matches
                         stage = stage_checksum;                 //    Go to the next stage
                         checksum = 0;                           //    Reset the checksum. 
                                                                 //        It takes a couple of bytes to get the checksum, 
@@ -112,21 +163,21 @@ namespace serializer{
                     }
                     break;
                 case stage_checksum:                            // We expect the checksum
-                    if(!checksum){                              // If this is the first byte of the checksum
-                        checksum = byte;                        //    Store it
-                    }
-                    else{                                       // If this is the second byte of the checksum
-                        checksum += byte<<8;                    //    Store it
-                        stage = prefix;                         //    Go back to the first stage
-                        if(checksum == compute_checksum()){     //    If the checksum matches           
-                            return &object;                     //        Return a pointer to the object
+                    checksum = incoming_byte;                   //    Store it
+                    stage = stage_checksum_2;                   //    Advance to stage_checksum_2
+                    break;
+                case stage_checksum_2:                          // We expect the most significant byte of the checksum
+                    checksum += incoming_byte<<8;               //    Store it
+                    stage = stage_prefix;                       //    Go back to the first stage
+                    if(checksum == compute_checksum()){         //    If the checksum matches           
+                        if(destination_object){                 //        If we have a valid pointer to an object
+                            *destination_object = object;       //            Write the object to the given address
                         }
-                        else{                                   //    If the checksum doesn't match
-                            return nullptr;                     //        Return a null pointer
-                        }
+                        return true;                            //        Return true
                     }
                     break;
             } // end of switch
+            return false;                                       // If we get here, we haven't received a proper object yet
         } // end of deserialize
 
 
@@ -139,8 +190,8 @@ namespace serializer{
     bool
     printTo(Print &serial) const
     {
-        if(serial.)
-        serialize(serial.write);
+        if(!serial) return false;
+        serialize_instance.serialize<Stream, void>(Serial, &Stream::write);
     } // end printTo
 
     /// @brief (TODO test) Reads the object from the given serial port (or other Stream object), blocking until the object is received.
@@ -177,8 +228,8 @@ namespace serializer{
 
         /// @brief Sets the object to the given value and recomputes the checksum
         /// @param object 
-        void operator =(const T &object){
-            object = object;
+        void operator =(const T &new_value){
+            object = new_value;
             checksum = compute_checksum();
         }
 
@@ -189,7 +240,7 @@ namespace serializer{
         private:
 
         uint16_t compute_checksum(){
-            uint8_t checksum = 0;
+            uint16_t checksum = 0;
             uint8_t * ptr = (uint8_t *) &object;
             for(int i = 0; i < sizeof(T); i++){
                 checksum += ptr[i];
---
+++ test/serializer/test_serializer.cc
@@ -0,0 +1,174 @@
+#include <gtest/gtest.h>
+#include "Serializer.hpp"
+#include <queue>
+#include <cstdint>
+#include <string.h>
+
+
+// Setup a minimal reproducible example of buffers that can be used to test the Serializer
+//     Broadcaster -> Broadcaster::buffer -> Broadcaster::receive() -> Receiver::receive()
+
+class Broadcaster{
+  std::queue<uint8_t> buffer;
+  public:
+  void send(uint8_t data){
+    buffer.push(data);
+  }
+  int16_t receive(){
+    if(buffer.empty()) return -1;
+    uint8_t data = buffer.front();
+    buffer.pop();
+    return data;
+  }
+};
+
+class Receiver{
+  private:
+  Broadcaster &broadcaster;
+  public:
+  Receiver(Broadcaster &broadcaster): broadcaster(broadcaster){}
+  int16_t receive(){
+    return broadcaster.receive();
+  }
+};
+
+
+
+
+
+// Serialize and deserialize one integer
+TEST(SerializerTests, Serialize_Int) {
+
+  // Create the test object
+  int a = 0xf005ba11;
+
+  // Make a print stream
+  Broadcaster broadcaster;
+
+  // Serialize the object and transmit the message over the print stream
+  serializer::Serializer<int> serializer(a, 0);
+  serializer.serialize<Broadcaster, void>(broadcaster, &Broadcaster::send);
+
+  // Receive the message
+  Receiver receiver(broadcaster);
+
+  // Deserialize the message
+  serializer::Serializer<int> deserializer(0, 0);
+  int a_deserialized = -1;
+  while(true){
+    int16_t read_result = receiver.receive();
+    if(read_result<0) break;
+    deserializer.deserialize((uint8_t) read_result, &a_deserialized);
+  }
+
+  // Check that the serialized/deserialized object is the same as the original
+  ASSERT_EQ(a, a_deserialized);
+} // end Serialize_Int
+
+
+
+
+
+
+// Serialize and deserialize one hundred integers
+TEST(SerializerTests, Serialize_Int_100x) {
+
+
+  // Make a print stream...
+  Broadcaster broadcaster;
+  // ... and the print stream reader
+  Receiver receiver(broadcaster);
+
+  // Create the serializer...
+  serializer::Serializer<int> serializer(0, 0);
+  // ... and the deserializer
+  serializer::Serializer<int> deserializer(0, 0);
+
+  // Create the test object
+  for(int i = 0; i < 100; i++){
+      // Serialize the object i and transmit the message over the print stream
+      serializer = i;
+      serializer.serialize<Broadcaster, void>(broadcaster, &Broadcaster::send);
+
+      int i_deserialized = 0xDEADBEEF;
+      while(true){
+        int16_t read_result = receiver.receive();
+        if(read_result<0) break;
+        deserializer.deserialize((uint8_t) read_result, &i_deserialized);
+      }
+
+      // Check that the serialized/deserialized object is the same as the original
+      ASSERT_EQ(i, i_deserialized);
+  }
+} // end Serialize_Int_100x
+
+
+
+
+
+
+
+
+
+
+
+// Serialize and deserialize one integer
+TEST(SerializerTests, Serialize_Struct) {
+
+  class TestClass{
+    public:
+    long data[10];
+    TestClass(){
+      for(int i = 0; i < 10; i++) data[i] = 0;
+    }
+    bool operator==(const TestClass &other) const{
+      for(int i = 0; i < 10; i++) if (data[i]!=other.data[i]) return false;
+      return true;
+    }
+  };
+
+  // Create the test object
+  // Make a test object that can be serialized
+  struct Foo{
+    int         a;
+    float       b;
+    long long   c;
+    uint8_t     d;
+    TestClass   e;
+  };
+
+  struct Foo foo;
+  foo.a = 0xC001BEA7;
+  foo.b = 3.14159265359;
+  foo.c = -1;
+  foo.d = 42;
+  foo.e.data[5] = -512;
+
+  // Make a print stream
+  Broadcaster broadcaster;
+
+  // Serialize the object and transmit the message over the print stream
+  serializer::Serializer<struct Foo> serializer(foo, 0);
+  serializer.serialize<Broadcaster, void>(broadcaster, &Broadcaster::send);
+
+  // Receive the message
+  Receiver receiver(broadcaster);
+
+  // Deserialize the message
+  serializer::Serializer<struct Foo> deserializer(0);
+  struct Foo foo_deserialized;
+  while(true){
+    int16_t read_result = receiver.receive();
+    if(read_result<0) break;
+    deserializer.deserialize((uint8_t) read_result, &foo_deserialized);
+  }
+
+  // Check that the serialized/deserialized object is the same as the original
+  ASSERT_EQ(foo.a, foo_deserialized.a);
+  ASSERT_EQ(foo.b, foo_deserialized.b);
+  ASSERT_EQ(foo.c, foo_deserialized.c);
+  ASSERT_EQ(foo.d, foo_deserialized.d);
+  ASSERT_EQ(foo.e, foo_deserialized.e);
+} // end Serialize_Int
+
+
